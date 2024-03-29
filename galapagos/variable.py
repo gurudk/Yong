49caaf6afd7c2ae2dc6a81c407744c5349bc03a3
gurudk <zhangyongji@126.com>
2024-03-29 16:26:42 +0800

    backward

--- galapagos/variable.py
+++ galapagos/variable.py
@@ -2,7 +2,10 @@ import numpy as np
 
 
 class Variable:
-    def __init__(self,data):
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError("{} is not surpported".format(type(data)))
         self.data = data
         self.grad = None
         self.creator = None
@@ -10,15 +13,31 @@ class Variable:
     def set_creator(self, func):
         self.creator = func
 
+    def cleargrad(self):
+        self.grad = None
+
     def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
         funcs = [self.creator]
         while funcs:
             f = funcs.pop()
-            x,y = f.input, f.output
-            x.grad = f.backward(y.grad)
 
-            if x.creator is not None:
-                funcs.append(x.creator)
+            gys = [output.grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
+
+                if x.creator is not None:
+                    funcs.append(x.creator)
+
         # f = self.creator
         # if f is not None:
         #     x = f.input
@@ -26,18 +45,21 @@ class Variable:
         #     x.backward()
 
 
-
-
 class Function:
-    def __call__(self, input):
-        x = input.data
-        y = self.forward(x)
-        output = Variable(y)
-        output.set_creator(self)
-        self.input = input
-        self.output = output
+    def __call__(self, *inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]
 
-        return output
+        for output in outputs:
+            output.set_creator(self)
+
+        self.inputs = inputs
+        self.outputs = outputs
+
+        return outputs if len(outputs) > 1 else outputs[0]
 
     def forward(self, x):
         raise NotImplementedError()
@@ -47,81 +69,70 @@ class Function:
 
 
 class Square(Function):
-    def forward(self,x):
-        return x**2
+    def forward(self, x):
+        return x ** 2
 
     def backward(self, gy):
-        x = self.input.data
-        gx = 2*x*gy
+        x = self.inputs[0].data
+        gx = 2 * x * gy
         return gx
 
 
 class Exp(Function):
-    def forward(self,x):
+    def forward(self, x):
         return np.exp(x)
 
     def backward(self, gy):
-        x = self.input.data
+        x = self.inputs[0].data
         gx = np.exp(x) * gy
         return gx
 
 
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+
+    def backward(self, gy):
+        return gy, gy
 
 
-data = np.array(1.0)
-x = Variable(data)
-print(x.data)
-
-x = Variable(np.array(10))
-f = Square()
-y = f(x)
-print(type(y))
-print(y.data)
-
-A=Square()
-B=Exp()
-C=Square()
-
-x = Variable(np.array(0.5))
-a = A(x)
-b = B(a)
-y = C(b)
-
-assert y.creator == C
-assert y.creator.input == b
-assert y.creator.input.creator == B
-assert y.creator.input.creator.input == a
-assert y.creator.input.creator.input.creator == A
-assert y.creator.input.creator.input.creator.input == x
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
 
 
+def square(x):
+    f = Square()
+    return f(x)
 
-print(y.data)
 
-y.grad = 1.0
-b.grad = C.backward(y.grad)
-a.grad = B.backward(b.grad)
-x.grad = A.backward(a.grad)
+def exp(x):
+    f = Exp()
+    return f(x)
 
-print(x.grad)
 
-y.grad = np.array(1.0)
+def add(x0, x1):
+    f = Add()
+    return f(x0, x1)
 
-C = y.creator
-b = C.input
-b.grad = C.backward(y.grad)
-print(b.grad)
 
-B = b.creator
-a = B.input
-a.grad = B.backward(b.grad)
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
 
-print(a.grad)
 
-y.grad = np.array(1.0)
+x = Variable(np.array(2))
+
+y = add(x, x)
 y.backward()
 print(x.grad)
 
-
-
-
+x.cleargrad()
+y = add(add(x, x), add(x, add(x, x)))
+y.backward()
+print(x.grad)
