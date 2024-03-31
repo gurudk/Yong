a5e80b203ab3f8cdf549cd392a742f29b8da6251
gurudk <zhangyongji@126.com>
2024-03-31 18:11:31 +0800

    core function

--- galapagos/variable.py
+++ galapagos/variable.py
@@ -1,30 +1,80 @@
+import weakref
+
 import numpy as np
 
 
 class Variable:
-    def __init__(self, data):
+    __array_priority = 200
+
+    def __init__(self, data, name=None):
         if data is not None:
             if not isinstance(data, np.ndarray):
                 raise TypeError("{} is not surpported".format(type(data)))
         self.data = data
+        self.name = name
         self.grad = None
         self.creator = None
+        self.generation = 0
+
+    @property
+    def shape(self):
+        return self.data.shape
+
+    @property
+    def ndim(self):
+        return self.data.ndim
+
+    @property
+    def size(self):
+        return self.data.size
+
+    @property
+    def dtype(self):
+        return self.data.dtype
+
+    def __add__(self, other):
+        return add(self, other);
+
+    def __mul__(self, other):
+        return mul(self, other)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        if self.data is None:
+            return 'variable(None)'
+
+        p = str(self.data).replace("\n", "\n" + " " * 9)
+        return "varaible(" + p + ")"
 
     def set_creator(self, func):
         self.creator = func
+        self.generation = func.generation + 1
 
     def cleargrad(self):
         self.grad = None
 
-    def backward(self):
+    def backward(self, retain_grad=False):
         if self.grad is None:
             self.grad = np.ones_like(self.data)
+
+        funcs = []
+        seen_set = set()
+
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.generation)
+
+        add_func(self.creator)
 
         funcs = [self.creator]
         while funcs:
             f = funcs.pop()
 
-            gys = [output.grad for output in f.outputs]
+            gys = [output().grad for output in f.outputs]
             gxs = f.backward(*gys)
             if not isinstance(gxs, tuple):
                 gxs = (gxs,)
@@ -36,7 +86,12 @@ class Variable:
                     x.grad = x.grad + gx
 
                 if x.creator is not None:
-                    funcs.append(x.creator)
+                    add_func(x.creator)
+
+            if not retain_grad:
+                # don't save grads for mediate variable
+                for y in f.outputs:
+                    y().grad = None
 
         # f = self.creator
         # if f is not None:
@@ -47,17 +102,21 @@ class Variable:
 
 class Function:
     def __call__(self, *inputs):
+        inputs = [as_variable(x) for x in inputs]
         xs = [x.data for x in inputs]
         ys = self.forward(*xs)
         if not isinstance(ys, tuple):
             ys = (ys,)
         outputs = [Variable(as_array(y)) for y in ys]
 
-        for output in outputs:
-            output.set_creator(self)
+        if Config.enable_backprop:
+            self.generation = max([x.generation for x in inputs])
 
-        self.inputs = inputs
-        self.outputs = outputs
+            for output in outputs:
+                output.set_creator(self)
+
+            self.inputs = inputs
+            self.outputs = [weakref.ref(output) for output in outputs]
 
         return outputs if len(outputs) > 1 else outputs[0]
 
@@ -97,10 +156,67 @@ class Add(Function):
         return gy, gy
 
 
+class Mul(Function):
+
+    def forward(self, x0, x1):
+        y = x0 * x1
+        return y
+
+    def backward(self, gy):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        return gy * x1, gy * x0
+
+
+class Neg(Function):
+
+    def forward(self, x):
+        return -x
+
+    def backward(self, gy):
+        return -gy
+
+
+class Sub(Function):
+    def forward(self, x0, x1):
+        return x0 - x1
+
+    def backward(self, gy):
+        return gy, -gy
+
+
+class Div(Function):
+    def forward(self, x0, x1):
+        return x0 / x1
+
+    def backward(self, gy):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        return gy / x1, -gy * x0 / x1 ** 2
+
+
+class Pow(Function):
+    def forward(self, x0, x1):
+        return x0 ** x1
+
+    def backward(self, gy):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+
+        return x1 * x0 ** (x1 - 1) * gy
+
+
+class Config:
+    enable_backprop = True
+
+
 def as_array(x):
     if np.isscalar(x):
         return np.array(x)
     return x
+
+
+def as_variable(obj):
+    if isinstance(obj, Variable):
+        return obj
+    return Variable(obj)
 
 
 def square(x):
@@ -114,8 +230,58 @@ def exp(x):
 
 
 def add(x0, x1):
+    x1 = as_array(x1)
     f = Add()
     return f(x0, x1)
+
+
+def mul(x0, x1):
+    x1 = as_array(x1)
+    return Mul()(x0, x1)
+
+
+def sub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x0, x1)
+
+
+def rsub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x1, x0)
+
+
+def div(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x0, x1)
+
+
+def rdiv(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x1, x0)
+
+
+def pow(x0, x1):
+    x1 = as_array(x1)
+    return Pow()(x0, x1)
+
+
+def rpow(x0, x1):
+    x1 = as_array(x1)
+    return Pow()(x1, x0)
+
+
+def neg(x):
+    return Neg()(x)
+
+
+Variable.__radd__ = add
+Variable.__rmul__ = mul
+Variable.__neg__ = neg
+Variable.__sub__ = sub
+Variable.__rsub__ = rsub
+Variable.__truediv__ = div
+Variable.__rtruediv__ = rdiv
+Variable.__pow__ = pow
 
 
 def numerical_diff(f, x, eps=1e-4):
@@ -126,13 +292,15 @@ def numerical_diff(f, x, eps=1e-4):
     return (y1.data - y0.data) / (2 * eps)
 
 
-x = Variable(np.array(2))
+e = -2
+print(2 ** e)
+print(e ** 2)
 
-y = add(x, x)
-y.backward()
-print(x.grad)
-
-x.cleargrad()
-y = add(add(x, x), add(x, add(x, x)))
-y.backward()
-print(x.grad)
+a = Variable(np.array(2))
+b = -a
+c = b ** 3
+# d = c - 2 / b - a / 2 ** a
+d = exp(c)
+print(d)
+d.backward()
+print(a.grad)
