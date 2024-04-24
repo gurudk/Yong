d46f1d6363d02689896d9368dcccb358e51e80e7
gurudk <zhangyongji@126.com>
2024-04-24 14:33:50 +0800

    resnet

---
+++ deeplearning/conv/conv2d.py
@@ -0,0 +1,18 @@
+import torch
+from torch import nn
+
+m = nn.Conv2d(16, 2, 3, stride=2)
+m = nn.Conv2d(16, 2, (3, 5), stride=(2, 1), padding=(4, 2))
+m = nn.Conv2d(16, 33, (3, 5), stride=(2, 1), padding=(4, 2), dilation=(3, 1))
+
+input = torch.randn(20, 16, 50, 100)
+output = m(input)
+
+print(output.shape)
+
+m = nn.BatchNorm2d(3)
+
+input = torch.randn(2, 3, 3, 4)
+output = m(input)
+print(input)
+print(output)
