b66a3b6a5b72d5158da73c87d3ef04acc50ff22e
gurudk <zhangyongji@126.com>
2024-07-31 17:00:04 +0800

    pyside6 study code

--- deeplearning/annotedgui/pyside6_toolbar.py
+++ deeplearning/annotedgui/pyside6_toolbar.py
@@ -1,18 +1,23 @@
 import sys
-from PySide6.QtWidgets import (
-    QMainWindow, QApplication,
-    QLabel, QToolBar, QStatusBar
-)
+import sys
+
+from PySide6.QtCore import QSize, Qt
 from PySide6.QtGui import QAction, QIcon
-from PySide6.QtCore import Qt, QSize
+from PySide6.QtWidgets import (
+    QApplication,
+    QCheckBox,
+    QLabel,
+    QMainWindow,
+    QStatusBar,
+    QToolBar,
+)
 
 
 class MainWindow(QMainWindow):
-
     def __init__(self):
-        super(MainWindow, self).__init__()
+        super().__init__()
 
-        self.setWindowTitle("My Awesome App")
+        self.setWindowTitle("My App")
 
         label = QLabel("Hello!")
         label.setAlignment(Qt.AlignCenter)
@@ -23,13 +28,33 @@ class MainWindow(QMainWindow):
         toolbar.setIconSize(QSize(16, 16))
         self.addToolBar(toolbar)
 
-        button_action = QAction(QIcon("animal-monkey.png"), "Your button", self)
+        button_action = QAction(QIcon("bug.png"), "&Your button", self)
         button_action.setStatusTip("This is your button")
         button_action.triggered.connect(self.onMyToolBarButtonClick)
         button_action.setCheckable(True)
         toolbar.addAction(button_action)
 
+        toolbar.addSeparator()
+
+        button_action2 = QAction(QIcon("bug.png"), "Your &button2", self)
+        button_action2.setStatusTip("This is your button2")
+        button_action2.triggered.connect(self.onMyToolBarButtonClick)
+        button_action2.setCheckable(True)
+        toolbar.addAction(button_action2)
+
+        toolbar.addWidget(QLabel("Hello"))
+        toolbar.addWidget(QCheckBox())
+
         self.setStatusBar(QStatusBar(self))
+
+        menu = self.menuBar()
+
+        file_menu = menu.addMenu("&File")
+        file_menu.addAction(button_action)
+        file_menu.addSeparator()
+
+        file_submenu = file_menu.addMenu("Submenu")
+        file_submenu.addAction(button_action2)
 
     def onMyToolBarButtonClick(self, s):
         print("click", s)
