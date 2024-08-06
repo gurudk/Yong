8b6dea946cef03888968314cbdce0060eb1157df
gurudk <zhangyongji@126.com>
2024-08-06 17:24:37 +0800

    vit pytorch test

--- deeplearning/vit/image_trans.py
+++ deeplearning/vit/image_trans.py
@@ -2,4 +2,4 @@ from PIL import Image
 
 img = Image.open("6.png")
 imgr = img.resize((256, 144))
-imgr.save("6mini.png")
+imgr.save("6_256.png")
--- deeplearning/vit/vit_pytorch_test.py
+++ deeplearning/vit/vit_pytorch_test.py
@@ -1,1 +1,26 @@
+import torch
+from vit_pytorch import ViT
+import time
 
+v = ViT(
+    image_size=256,
+    patch_size=16,
+    num_classes=4,
+    dim=512,
+    depth=6,
+    heads=8,
+    mlp_dim=1024,
+    dropout=0.1,
+    emb_dropout=0.1
+)
+
+start = time.time()
+
+for i in range(100):
+    img = torch.randn(1, 3, 256, 144)
+
+    preds = v(img)  # (1, 1000)
+
+end = time.time()
+print("Call duration:", str((end - start) / 100))
+print(preds.shape)
