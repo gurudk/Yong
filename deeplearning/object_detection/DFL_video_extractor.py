40c039961bf4c76bc8b1f8d79ad1de1b1110dc86
gurudk <zhangyongji@126.com>
2024-09-19 15:25:50 +0800

    Video frames extractor

---
+++ deeplearning/object_detection/DFL_video_extractor.py
@@ -0,0 +1,75 @@
+# Importing all necessary libraries
+import cv2
+import os
+import pathlib
+from pathlib import Path
+
+
+def generate_frames(video_file_path, out_dir_path):
+    currentframe = 0
+    cam = cv2.VideoCapture(str(video_file_path))
+    while True:
+
+        # reading from frame
+        ret, frame = cam.read()
+
+        if ret:
+            # if video is still left continue creating images
+            video_file_stem = video_file_path.stem
+            out_frames_dir_path = out_dir_path.joinpath(video_file_stem)
+            if not out_frames_dir_path.exists():
+                out_frames_dir_path.mkdir(parents=True, exist_ok=True)
+
+            img_file_path = out_frames_dir_path.joinpath(video_file_stem + "_" + str(currentframe) + ".png")
+
+            # writing the extracted images
+            cv2.imwrite(str(img_file_path), frame)
+            print(img_file_path, " created~")
+
+            # increasing counter so that it will
+            # show how many frames are created
+            currentframe += 1
+        else:
+            break
+
+    # Release all space and windows once done
+    cam.release()
+
+
+DFL_DIR = "/home/wolf/datasets/DFL/train/"
+GEN_DIR = "/home/wolf/datasets/DFL/images/"
+
+
+# for root, dirs, files in os.walk(DFL_DIR):
+#     print(sorted(files))
+#     # print(dirs)
+
+# root_dir = pathlib.Path(DFL_DIR)
+#
+# # for item in root_dir.iterdir():
+# #     if item.is_file():
+# #         print(item)
+#
+# allfiles = sorted(list(root_dir.rglob("*")))
+#
+# print(len(allfiles))
+#
+# for file_path in allfiles:
+#     if file_path.suffix.endswith("mp4"):
+#         vfile_dir = Path(GEN_DIR).joinpath(file_path.stem)
+#         if not vfile_dir.exists():
+#             vfile_dir.mkdir(parents=True, exist_ok=True)
+#
+#         # open the mp4
+#         # print(str(file_path))
+#         generate_frames(file_path, Path(GEN_DIR))
+#         break
+
+def temp_gen_path(file_name):
+    stem = file_name.split(" ")[0]
+    train_dir = Path(DFL_DIR)
+    file_path = train_dir.joinpath(stem).joinpath(file_name)
+    return file_path
+
+
+generate_frames(temp_gen_path("B1606b0e6_1 (21).mp4"), Path(GEN_DIR))
