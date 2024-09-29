9dfee611f3197d2d5dfe2a43ab1f75de3cac6c64
gurudk <zhangyongji@126.com>
2024-09-29 17:17:53 +0800

    try transformer, on the road

---
+++ deeplearning/vit/player_orientation_data_explore_stat.py
@@ -0,0 +1,20 @@
+import json
+import numpy as np
+import torch
+
+file = "./explored/loss25_data_explored_file_140pth_angle.txt.20240929153227"
+val_file = "./explored/loss2555555_val_data_explored_file_140pth_angle.txt.20240929155538"
+
+with open(val_file, 'r') as rf:
+    jd = json.loads(rf.read())
+
+print(len(jd))
+
+list1 = list(filter(lambda x: x[1] < 1.5, jd))
+print(len(list1))
+
+np1 = np.array(list1)
+
+a = np.float32(np1[:, 1])
+
+print(a.mean())
