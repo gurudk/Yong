5ab6817f401f0f6246a820dea135ae65a4757dee
gurudk <zhangyongji@126.com>
2024-10-02 15:23:13 +0800

    batch view range algorithm

--- deeplearning/vit/player_orientation_data_explore_stat.py
+++ deeplearning/vit/player_orientation_data_explore_stat.py
@@ -5,12 +5,12 @@ import torch
 file = "./explored/loss25_data_explored_file_140pth_angle.txt.20240929153227"
 val_file = "./explored/loss2555555_val_data_explored_file_140pth_angle.txt.20240929155538"
 
-with open(val_file, 'r') as rf:
+with open(file, 'r') as rf:
     jd = json.loads(rf.read())
 
 print(len(jd))
 
-list1 = list(filter(lambda x: x[1] < 1.5, jd))
+list1 = list(filter(lambda x: x[1] < 0.5, jd))
 print(len(list1))
 
 np1 = np.array(list1)
