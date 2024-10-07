60b84b71d973e7d5d4ca5a7c94a3d2d09494a140
gurudk <zhangyongji@126.com>
2024-10-07 17:18:37 +0800

    vae papers

--- deeplearning/soccernet/soccernet_spotting.py
+++ deeplearning/soccernet/soccernet_spotting.py
@@ -1,5 +1,5 @@
 from SoccerNet.Downloader import SoccerNetDownloader
 
 mySoccerNetDownloader = SoccerNetDownloader(LocalDirectory="/home/wolf/datasets/SoccerNet")
-mySoccerNetDownloader.downloadDataTask(task="spotting-2023", split=["train", "valid", "test", "challenge"])
+mySoccerNetDownloader.downloadDataTask(task="spotting-2023", split=["valid", "test", "challenge"])
 # mySoccerNetDownloader.downloadDataTask(task="spotting-ball-2023", split=["train", "valid", "test", "challenge"])
--- deeplearning/soccernet/soccernet_tracking.py
+++ deeplearning/soccernet/soccernet_tracking.py
@@ -2,4 +2,4 @@ from SoccerNet.Downloader import SoccerNetDownloader
 
 mySoccerNetDownloader = SoccerNetDownloader(LocalDirectory="/home/wolf/datasets/SoccerNet")
 # mySoccerNetDownloader.downloadDataTask(task="tracking", split=["train", "test", "challenge"])
-mySoccerNetDownloader.downloadDataTask(task="tracking-2023", split=["train", "test", "challenge"])
+mySoccerNetDownloader.downloadDataTask(task="tracking-2023", split=["valid"])
