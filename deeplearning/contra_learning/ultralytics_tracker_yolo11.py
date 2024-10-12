cee5f7f28d7569f25715cf478929802cc549e89d
gurudk <zhangyongji@126.com>
2024-10-12 17:24:28 +0800

    ultralytic tracker

---
+++ deeplearning/contra_learning/ultralytics_tracker_yolo11.py
@@ -0,0 +1,39 @@
+import cv2
+
+from ultralytics import YOLO
+
+cv2.namedWindow('displaymywindows', cv2.WINDOW_NORMAL)
+
+# Load the YOLO11 model
+model = YOLO("yolo11n.pt")
+
+# Open the video file
+video_path = "xueshi_new_193.mp4"
+cap = cv2.VideoCapture(video_path)
+
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        # Run YOLO11 tracking on the frame, persisting tracks between frames
+        results = model.track(frame, persist=True)
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        cv2.imshow("YOLO11 Tracking", annotated_frame)
+
+        # cv2.waitkey(0)
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
