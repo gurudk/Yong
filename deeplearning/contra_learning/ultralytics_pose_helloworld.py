8849a31f7cd3ea17638f807aa99e797e19bbe97d
gurudk <zhangyongji@126.com>
2024-11-03 18:04:11 +0800

    little change

--- deeplearning/contra_learning/ultralytics_pose_helloworld.py
+++ deeplearning/contra_learning/ultralytics_pose_helloworld.py
@@ -5,7 +5,8 @@ model = YOLO("yolo11n-pose.pt")  # load an official model
 # model = YOLO("path/to/best.pt")  # load a custom model
 
 # Predict with the model
-results = model("/home/wolf/datasets/reid/frames/SR583/SR583_0/SR583_0_0.png", show=True,
+results = model("/home/wolf/datasets/reid/dataset/test_final_dataset_32_128/9/8_SR583_0_111_frame_841_823.png",
+                show=True,
                 save=True)  # predict on an image
 
 print(results)
