ddc41448cc9bfa8ce2322982ae9e7bb3790f70e6
gurudk <zhangyongji@126.com>
2024-12-09 16:29:30 +0800

    some papers

---
+++ deeplearning/stablediffusion/sd21_test.py
@@ -0,0 +1,14 @@
+import torch
+from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
+
+model_id = "stabilityai/stable-diffusion-2-1"
+
+# Use the DPMSolverMultistepScheduler (DPM-Solver++) scheduler here instead
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+pipe = pipe.to("cuda")
+
+prompt = "a photo of a soccer player wearing a red number 13 jersey on a football field playing a ball"
+image = pipe(prompt).images[0]
+
+image.save("astronaut_rides_horse.png")
