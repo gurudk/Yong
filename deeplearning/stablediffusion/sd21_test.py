a67ba7e37ef44297b6a4296c4ca18ae8e85bf635
gurudk <zhangyongji@126.com>
2024-12-18 16:17:09 +0800

    some papers

--- deeplearning/stablediffusion/sd21_test.py
+++ deeplearning/stablediffusion/sd21_test.py
@@ -8,7 +8,7 @@ pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 pipe = pipe.to("cuda")
 
-prompt = "a photo of a soccer player wearing a red number 13 jersey on a football field playing a ball"
+prompt = "a photograph of a soccer player facing backward  wearing number 13 jersey named \"Zhang\" in a big football field, full body photo, white football shoes, no deformation"
 image = pipe(prompt).images[0]
 
 image.save("astronaut_rides_horse.png")
