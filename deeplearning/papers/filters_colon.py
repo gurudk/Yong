717a22ce8c004ce62e890e784353ed168aa67ed0
gurudk <zhangyongji@126.com>
2024-12-30 13:14:55 +0800

    some papers

---
+++ deeplearning/papers/filters_colon.py
@@ -0,0 +1,15 @@
+work_dir = "/home/wolf/papers_backup"
+
+from pathlib import Path
+import os
+
+work_path = Path(work_dir)
+
+for f in work_path.rglob("*.pdf"):
+    # print(f)
+    file_name = f.name
+    if ":" in str(f):
+        new_name = file_name.replace(":", "_")
+        print(new_name)
+        print(f)
+        f.rename(new_name)
